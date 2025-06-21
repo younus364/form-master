2e08efabdcb637f8b390416c839e1e4d5b1d91ed
younus364 <younussheikh364@gmail.com>
2025-06-21 11:52:05 +0600

    Create a form

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import SimpleForm from './compnents/SimpleFrom/SimpleForm'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+     <SimpleForm></SimpleForm>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/compnents/SimpleFrom/SimpleForm.jsx
@@ -0,0 +1,15 @@
+
+
+function SimpleForm(props) {
+    return (
+        <div>
+            <form >
+                <input type="text" name="" id="" />
+                <br />
+                <button>submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default SimpleForm;
